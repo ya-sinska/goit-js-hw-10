70dd4f3e3497e0d0fd5665152ce872d3a574f90c
Romanna Yasinska <r.bren.1991@gmail.com>
2022-01-08 03:03:49 +0200

    upd task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 // функція для отримання даних з бекенду по назві країни
 // повертає масив об єктів країн відповідно до запиту
 export function fetchCountries (contryName) {
-  return fetch(`https://restcountries.com/v3.1/name/${contryName}`)
+  return fetch(`https://restcountries.com/v3.1/name/${contryName}?fields=name,capital,population,flags,languages,fifa`)
     .then(r => {
         return r.json()  
     })
--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
 import './css/styles.css';
 import contryCard from './contry-card.hbs'
 import { fetchCountries } from './fetchCountries'
+import Notiflix from 'notiflix';
 
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
-    cardContainer: document.querySelector(".country-list")
+    cardContainer: document.querySelector(".country-list"),
+    input: document.querySelector('#search-box'),
+    countryInfo: document.querySelector('.country-info'),
+    countryList: document.querySelector('.country-list'),
 }
-fetchCountries('Uk')
-    .then(renderCountryCard)
-    .catch(error => {
-    console.log(error)
-    })
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
+
+function onInput(e) {
+    const inputValue = (e.target.value).trim();
+    console.log(inputValue)
+    if (!inputValue) {
+        refs.countryInfo.innerHTML = '';
+        refs.countryList.innerHTML = '';
+    } else {
+       fetchCountries(inputValue)
+        .then(renderCountryCard)
+        .catch(err => Notify.failure('Oops, there is no country with that name'));
+    }
     
+}
 
+function renderCountryCard(contries) {
+    if (contries.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (contries.length >= 2) {
+        Notiflix.Notify.info('ggg');
+    }
+    else {
+       const markup = contryCard(contries)
+    refs.cardContainer.innerHTML = markup    
+    }
+    
+}
 
-function renderCountryCard(contry) {
-    const markup = contryCard(contry)
-    console.log(markup)
-    refs.cardContainer.innerHTML = markup   
+function catchError(error) {
+    
+    Notiflix.Notify.failure('error');
 }
